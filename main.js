7f7c5c93c94ca07da56fa6bf68289ed0c174a8ce
Saanyak <80775808+Saanyak@users.noreply.github.com>
2021-10-20 14:32:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+prediction1 = "";
+prediction2 = "";
+
+Webcam.set({
+    width : 350,
+    height : 300, 
+    image_format : "png",
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version : ",ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iEdn0V9jO/model.json",modelLoaded);
+
+function modelLoaded(){
+    console("Model Loaded");
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    var speak_data1 = "The first prediction is "+prediction1;
+    var speak_data2 = "And the second prediction is "+prediction2;
+    var utterthis = new SpeechSynthesisUtterance(speak_data1+speak_data2);
+    synth.speak(utterthis);
+}
+
+function check_snapshot(){
+    var img = document.getElementById("captured_image");
+    classifier.classify(img, gotresults);
+}
+
+function gotresults(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction1 = results[0].label;
+        prediction2 = results[1].label;
+        speak();
+
+        if (results[0].label == "Great"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "Peace"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if (results[0].label == "Hand"){
+            document.getElementById("update_emoji").innerHTML = "&#128400;";
+        }
+        if (results[0].label == "Fist"){
+            document.getElementById("update_emoji").innerHTML = "&#9994;";
+        }
+
+        if (results[1].label == "Great"){
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if (results[1].label == "Peace"){
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if (results[1].label == "Hand"){
+            document.getElementById("update_emoji2").innerHTML = "&#128400;";
+        }
+        if (results[1].label == "Fist"){
+            document.getElementById("update_emoji2").innerHTML = "&#9994;";
+        }
+    }
+}
